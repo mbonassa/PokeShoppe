3e041dfe82086ccc898a3057e046afc97db8099c
mbonassa <marcelo.bonassa@gmail.com>
2017-06-06 12:02:46 -0400

    Update order.js

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Order = db.define('order', {
-    status: Sequelize.ENUM('active', 'closed')
+    status: Sequelize.ENUM('ACTIVE', 'CLOSED')
 });
 
 module.exports = Order;

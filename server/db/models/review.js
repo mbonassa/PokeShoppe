2d18871929163a2445a7dadfd6d18f0563b522ec
Shon-Elias <shon.elias@gmail.com>
2017-06-06 11:50:58 -0400

    insert validation in review and order models

--- server/db/models/review.js
+++ server/db/models/review.js
@@ -2,8 +2,19 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Review = db.define('review', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
     description: Sequelize.TEXT,
-    rating: SEQUELIZE.INTEGER
-})
+    rating: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        validate: {
+            min: 1,
+            max: 5
+        }
+    }
+});
 
-module.exports = Review; 
+module.exports = Review;

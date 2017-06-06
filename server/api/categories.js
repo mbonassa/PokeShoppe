459fad3d3bb7ffd1e4909f5f20cbada923073fa2
mbonassa <marcelo.bonassa@gmail.com>
2017-06-06 17:24:04 -0400

    Update categories.js

--- server/api/categories.js
+++ server/api/categories.js
@@ -2,23 +2,20 @@ const router = require('express').Router();
 const Category = require('../db').model('category');
 module.exports = router;
 
+//get all categories (admin should be able to)
 router.get('/', (req, res, next) => {
   Category.findAll()
     .then(foundCategories => {
-      res.send(foundCategories)
+      res.json(foundCategories)
     })
     .catch(next);
 })
 
 
 router.get('/:categoryId', (req, res, next) => {
-  Category.findOne( {
-    where: {
-      id: req.params.categoryId
-    }
-  })
+  Category.findById(req.params.categoryId)
     .then(foundCategory => {
-      res.send(foundCategory)
+      res.json(foundCategory)
     })
     .catch(next);
 })
@@ -26,17 +23,19 @@ router.get('/:categoryId', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   Category.create(req.body, {returning: true})
-    .then((createdRecord) => {
+    .then(createdRecord => {
       if (!createdRecord) res.sendStatus(400)
-      else res.sendStatus(201)
+      else res.status(201).json(createdRecord)
     })
     .catch(next);
 })
 
 
 router.put('/:categoryId', (req, res, next) => {
-  Category.update(req.body)
-    .then((affectedCount) => {
+  Category.update(req.body, {where: 
+      {id: req.params.categoryId}
+    })
+    .then(affectedCount => {
       if (!affectedCount[0]) res.sendStatus(400)
       else res.sendStatus(200)
     })
@@ -45,11 +44,7 @@ router.put('/:categoryId', (req, res, next) => {
 
 
 router.delete('/:categoryId', (req, res, next) => {
-  Category.destroy( {
-    where: {
-      id: req.params.categoryId
-    }
-  })
+  Category.destroyById(req.params.categoryId)
     .then(deletedCount => {
       if (!deletedCount) res.sendStatus(400)
       else res.sendStatus(200)

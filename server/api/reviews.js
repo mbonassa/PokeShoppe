1b69062ce717445392ee39d8e109878306cd8a72
Patrick Hoyt <ptrckhoyt120@gmail.com>
2017-06-08 15:55:52 -0400

    progress on review routes tests

--- server/api/reviews.js
+++ server/api/reviews.js
@@ -18,14 +18,21 @@ router.get('/:productId', (req, res, next) => {
 //Post a review to a specific product (from a specific user)  — /:productId
 router.post('/:productId', (req, res, next) => {
   Review.create(req.body, {returning: true})
+    // .then(createdRecord => {
+    //   console.log('CREATED RECORD!!!!',createdRecord)
+    //   return Promise.all(
+    //     createdRecord.setProduct(req.params.productId, {returning: true}),
+    //     createdRecord.setUser(req.body.userId, {returning: true})
+    //   )
+    // })
+    // .then(returnedArray => {
+    //   console.log('RETURNED ARRAY',returnedArray)
+    //   if (!returnedArray[1]) res.sendStatus(404)
+    //   else res.sendStatus(201)
+    // })
     .then(createdRecord => {
-      return Promise.all(
-        createdRecord.setProduct(req.params.productId, {returning: true}),
-        createdRecord.setUser(req.body.userId, {returning: true})
-      )
-    })
-    .then(returnedArray => {
-      if (!returnedArray[1]) res.sendStatus(404)
+      console.log('CREATED RECORD',createdRecord)
+      if (!createdRecord) res.sendStatus(404)
       else res.sendStatus(201)
     })
     .catch(next);
--- test/review.routes.spec.js
+++ test/review.routes.spec.js
@@ -1,6 +1,10 @@
 const db = require('../server/db/db');
 const Review = require('../server/db/models/review.js');
-import app from '../server/api';
+const Product = require('../server/db/models/product.js');
+const User = require('../server/db/models/user.js');
+//const app = require('../server/api')
+
+import app from '../server';
 
 // import fsMisc from 'fs-misc';
 import chai from 'chai';
@@ -9,7 +13,7 @@ import chai from 'chai';
 // chai.use(chaiProperties);
 // chai.use(chaiThings);
 const expect = chai.expect;
-import supertest from 'supertest-as-promised';
+import supertest from 'supertest';
 // import sinon from 'sinon';
 
 
@@ -29,46 +33,106 @@ describe('▒▒▒ Backend tests ▒▒▒', () => {
             agent = supertest(app);
         });
 
-
         describe('api routes', () => {
+
+            let user1;
+            let user2;
+            let user3;
+            beforeEach('Seed users', () => {
+                const users = [
+                  {name: 'Peter', email: 'peter.griffin.the2nd@gmail.com', password: 'peter'}, {name: 'Lois', email: 'lois.griffin.the2nd@gmail.com', password: 'lois'}, {name: 'Meg', email: 'meg.griffin.the2nd@gmail.com', password: 'meg'}
+                ];
+                return User.bulkCreate(users, {returning: true})
+                    .then(createdUsers => {
+                        user1 = createdUsers[0].dataValues.id;
+                        user2 = createdUsers[1].dataValues.id;
+                        user3 = createdUsers[2].dataValues.id
+                    });
+
+            });
+
+            let product1;
+            let product2;
+            let product3;
+            beforeEach('Seed products', () => {
+                const products = [
+                  {name: 'Pikachu', description:  'Pika-pika-pikachu', category: 'yellow', inventory_qty: 10, price: 99.99 },
+                  {name: 'Celebi', description:  'Ce-celebi', category: 'greenish', inventory_qty: 20, price: 19.99 },
+                  {name: 'Eevee', description:  'haha-Eevee', category: 'brown', inventory_qty: 30, price: 39.99 }
+                ];
+                return Product.bulkCreate(products, {returning: true})
+                    .then(createdProducts => {
+                        // console.log('CREATED PRODUCTS!!!!',createdProducts)
+                        product1 = createdProducts[0].dataValues.id;
+                        product2 = createdProducts[1].dataValues.id;
+                        product3 = createdProducts[2].dataValues.id
+                    });
+
+            });
+
 
             let review1;
             let review2;
             let review3;
             beforeEach('Seed reviews', () => {
                 const reviews = [
-                  {title: 'Super cool product', description: 'I bought it for my son and he plays with it all the time', rating: 5},
-                  {title: 'Bad product', description: 'I bought it for my son and he does not plays with it at all', rating: 1},
-                  {title: 'Great product', description: 'I bought for myself', rating: 4},
+                  {title: 'Super cool product', description: 'I bought it for my son and he plays with it all the time', rating: 5, productId: 1},
+                  {title: 'Bad product', description: 'I bought it for my son and he does not plays with it at all', rating: 1, productId: 1},
+                  {title: 'Great product', description: 'I bought for myself', rating: 4, productId: 2},
                 ];
                 return Review.bulkCreate(reviews, {returning: true})
                     .then(createdReviews => {
-                        // console.log('CREATED REVIEWS', createdReviews)
-                        review1 = createdReviews[0].id;
-                        review2 = createdReviews[1].id;
-                        review3 = createdReviews[1].id
+                        // console.log('CREATED REVIEWS', createdReviews[0].dataValues)
+                        review1 = createdReviews[0].dataValues.title;
+                        review2 = createdReviews[1].dataValues.title;
+                        review3 = createdReviews[2].dataValues.id
+                        // console.log('REVIEW1',review1)
                     });
 
             });
 
-            describe('reviews', () => {
+            describe('get', () => {
 
                 it('Get all reviews from a specific product — /:productId', () => {
-                    // console.log('HERE!!!')
                     return agent
-                        .get('/reviews/1')
+                        .get('/api/reviews/1')
                         .expect(200)
                         .then(res => {
-                            console.log('HERE!!!',res)
                             expect(res.body).to.be.an('array');
-                            expect(res.body.length).to.be.equal(3);
-                            expect(res.body).to.contain.a.thing.with('id', review1);
-                            expect(res.body).to.contain.a.thing.with('id', review2);
+                            expect(res.body.length).to.be.equal(2);
+                            expect(res.body[0]).to.be.an('object').that.includes({
+                            title: 'Super cool product'
+                            });
+                            expect(res.body[1]).to.be.an('object').that.includes({
+                            title: 'Bad product'
+                            });
                         });
                 });
-            })
+            });
 
-        })
-      })
-    })
-})
+            describe('post', () => {
+
+                it('Post a review to a specific product (from a specific user)  — /:productId', () => {
+                    return agent
+                        .post('/api/reviews/2')
+                        .send({
+                            title: 'good for me!',
+                            description: "I like this thing... it is so useful I can't even take it",
+                            rating: 4,
+                            userId: 1
+                        })
+                        .expect(201)
+                        .then(res => {
+                            console.log('RES!!!',res)
+                            const createdReview = res.body;
+                            return Review.findById(createdReview.id)
+                        })
+                        .then(foundReview => {
+                            expect(foundReview.description).to.be.equal("I like this thing... it is so useful I can't even take it");
+                        })
+                });
+            });
+        });
+      });
+    });
+});

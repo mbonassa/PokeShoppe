36e62e84abd4e5ee676f2f9fac4e8d10928f5477
Mike van Genderen <mikevg466>
2017-06-12 14:14:41 -0400

    refactor: splits main into ProductListContainer to separate from SingleProductContainer

--- client/components/Main.js
+++ client/components/Main.js
@@ -1,60 +1,15 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Link } from 'react-router';
-import { logout } from '../reducer/user';
 import Navbar from './Navbar';
-import { fetchProducts } from '../reducer/product';
-import ProductItem from './ProductItem';
-import SingleProduct from './SingleProduct';
 
 // Component //
 
-
-class Main extends React.Component {
-
-  constructor (props) {
-    super(props);
-
-  }
-
-  componentDidMount() {
-    this.props.onEnter()
-  }
-
-  render (props) {
-    //const { children, handleClick, loggedIn } = props;
-    return (
-      <div>
-        <Navbar />
-        {this.props.children}
-        <ul>
-          {
-            this.props.product.listProducts
-              .map(product => <ProductItem singleProduct={product} key={product.id} />)
-          }
-        </ul>
-      </div>
-    );
-  }
+const Main = (props) => {
+  return (
+    <div>
+      <Navbar />
+      {props.children}
+    </div>
+  )
 }
 
-// Main.propTypes = {
-//   children: PropTypes.object,
-//   handleClick: PropTypes.func.isRequired,
-//   loggedIn: PropTypes.bool.isRequired
-// };
-
-
-// // Container //
-
-const mapState = (state) => state;
-
-const mapDispatch = dispatch => ({
-  onEnter: () => {
-    return dispatch(fetchProducts())
-  },
-});
-
-export default connect(mapState, mapDispatch)(Main);
-
+export default Main;

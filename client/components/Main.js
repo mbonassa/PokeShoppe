e46413709b9525453b8bef9482aebdea017bb94d
Marcelo Bonassa <marcelo.bonassa@gmail.com>
2017-06-12 13:41:18 -0400

    feature: add to cart functionality from home page, with associated duck file

--- client/components/Main.js
+++ client/components/Main.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router';
 import { logout } from '../reducer/user';
 import Navbar from './Navbar';
-import { fetchProducts } from '../reducer/product';
+import { fetchProducts, fetchCart } from '../reducer/product';
 import ProductItem from './ProductItem';
 
 // Component //
@@ -17,6 +17,8 @@ class Main extends React.Component {
 
   componentDidMount() {
     this.props.onEnter()
+    //note: we're using a dummy userId before we finalize the logic
+    this.props.fetchingCart(5)
   }
 
   render () {
@@ -50,6 +52,9 @@ const mapDispatch = dispatch => ({
   onEnter: () => {
     return dispatch(fetchProducts())
   },
+  fetchingCart: userId => {
+    return dispatch(fetchCart(userId))
+  }
 });
 
 export default connect(mapState, mapDispatch)(Main);
--- client/reducer/index.js
+++ client/reducer/index.js
@@ -3,10 +3,12 @@ import user from './user';
 import order from './order';
 import product from './product';
 import review from './review';
+import cart from './cart';
 
 export default combineReducers({
    user,
    order,
    product,
-   review
+   review,
+   cart
   });

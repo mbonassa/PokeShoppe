78efe3474f6a066135c913a06ca6fda992219d69
mikevg466 <mikevg466@gmail.com>
2017-06-12 13:02:10 -0400

    Create Main.js

--- client/components/Main.js
+++ client/components/Main.js
@@ -24,7 +24,6 @@ class Main extends React.Component {
 
   render (props) {
     //const { children, handleClick, loggedIn } = props;
-    console.log('PROPS', this.props)
     return (
       <div>
         <Navbar />

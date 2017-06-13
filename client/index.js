13811002a4850868fefc8109d554c4f3482341b1
Patrick Hoyt <ptrckhoyt120@gmail.com>
2017-06-13 14:48:13 -0400

    fix: require login issue

--- client/index.js
+++ client/index.js
@@ -27,14 +27,14 @@ ReactDOM.render(
     <Router history={browserHistory}>
       <Route path="/" component={Main}>
         <IndexRoute component={Main} />
-        <Route onEnter={requireLogin}>
+
           <Route path="products" component={ProductListContainer} />
           <Route path="products/:id" component={SingleProduct} />
           <Route path="login" component={Login} />
           <Route path="signup" component={Signup} />
           <Route path="dashboard" />
           <Route path="cart" component={CartContainer}/>
-        </Route>
+
         <IndexRedirect to="products" />
       </Route>
     </Router>
